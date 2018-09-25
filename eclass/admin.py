bae7555bc6cb4a4abceb8bfc5cd902d1cc20c8da
SDX <realsdx0@gmail.com>
2018-09-25 22:09:30 +0530

    Upadted models

--- eclass/admin.py
+++ eclass/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from eclass import models
 
 # Register your models here.
+admin.site.register(models.Assignment)
+admin.site.register(models.Student)
+admin.site.register(models.Teacher)
+admin.site.register(models.Problem)
+admin.site.register(models.CodeClass)
--- eclass/models.py
+++ eclass/models.py
@@ -2,18 +2,57 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Problems(models.Model):
-    """This hold all types of Problems, including custom ones"""
-    c_statement = models.TextField(blank=True, null=True)
-    
-    problem_uri = models.URLField(blank=True, null=True)
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    classes = models.CharField(max_length=512, null=True, blank=True) #Store a json list of class ids
 
-class Assignment(models.Model):
-    question = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.name
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
     class_enrolled = models.CharField(max_length=256, default="NE") #NE=NotEnrolled
     qs_assigned = models.CharField(max_length=256, default="NA") #NA=NotAssigned
     ans_submited = models.CharField(max_length=256, default="NS") #NS=NoneSubmited
+    total_score = models.FloatField(default=0.00)
+
+    def __str__(self):
+        return self.name
+
+
+class Problem(models.Model):
+    """This hold all types of Problems, including custom ones, a teacher can add to it."""
+    problem_statement = models.TextField(blank=True, null=True)
+    problem_uri = models.URLField(blank=True, null=True)
+
+class Assignment(models.Model):
+    """This model only represents each assignment given to a student"""
+    title = models.CharField(max_length=256)
+    question = models.TextField(blank=True, null=True)
+    assigned_to = models.ForeignKey(Student, on_delete=models.CASCADE, null=True, blank=True)
+
+    submission = models.TextField(blank=True, null=True)
+    is_submited = models.BooleanField(default=False)
+    attachment = models.FileField(upload_to='media/',null=True, blank=True)
+    due_by = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class CodeClass(models.Model):
+    class_name = models.CharField(max_length=255)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, null=True, blank=True)
+    student = models.ManyToManyField(Student, blank=True)
+    # enrolled_students = models.CharField(max_length=512, blank=True, null=True)
+     # Store a json list of student ids 
+    qs_list = models.CharField(max_length=512, blank=True, null=True) #Store a json list of qs/assignment ids
+
+
+    def __str__(self):
+        return self.class_name
+
+
 
